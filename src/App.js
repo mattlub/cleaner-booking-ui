ac55d3fd87e7a2c503f9e0aa92761d259dd64bc3
matt lubel <matt_lubel@hotmail.com>
2017-10-11 16:14:18 +0100

    add getAvailability functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
-import './App.css'
+import qs from 'querystring'
 import TimeInput from './components/TimeInput'
+import './App.css'
+import { availabilityUrl } from './constants'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      selectedPeriod: 2
+      selectedPeriod: 2,
+      isLoadingData: false,
+      availabilityData: null
     }
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+  }
+
+  getAvailability () {
+    this.setState({
+      isLoadingData: true
+    })
+    const queries = {
+      weekBeginning: '2016-12-05',
+      visitDuration: this.state.selectedPeriod,
+      postcode: 'EC1R 3BU'
+    }
+    const url = availabilityUrl + qs.stringify(queries)
+    return fetch(url)
+      .then(response => response.json())
   }
 
   handleInputChange (e) {
-    console.log(e.target.value)
     this.setState({
       selectedPeriod: e.target.value
     })
+  }
+
+  handleFormSubmit (e) {
+    e.preventDefault()
+    this.getAvailability()
+      .then(data => this.setState({
+        isLoadingData: false,
+        availabilityData: data
+      }))
   }
 
   render () {
@@ -31,10 +58,19 @@ class App extends Component {
           Book a cleaner.
         </h2>
 
-        <TimeInput
-          value={ this.state.selectedPeriod }
-          handleInputChange={ this.handleInputChange }
-        />
+        <form onSubmit={ this.handleFormSubmit } >
+          <TimeInput
+            value={ this.state.selectedPeriod }
+            handleInputChange={ this.handleInputChange }
+          />
+          <button type="submit">
+            Find a cleaner!
+          </button>
+        </form>
+
+        { this.state.isLoadingData &&
+          <span>Loading Results...</span>
+        }
 
       </div>
     )

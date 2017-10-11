137feb0746a4ce2cddf1700effc1b18dde88f67f
matt lubel <matt_lubel@hotmail.com>
2017-10-11 16:13:31 +0100

    convert TimeInput back to not being a form

--- src/components/TimeInput.js
+++ src/components/TimeInput.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { formatHours } from '../helpers'
 
-const TimeInput = ({ value, handleInputChange }) => {
-  return (
-    <form>
-      <label>Please select your required cleaning period:</label>
-      <input type="range" min="0.5" max="6" step="0.5"
-        defaultValue={ value }
-        onChange={ e => handleInputChange(e) }
-      />
-      <span>{ formatHours(value) }</span>
-
-    </form>
-  )
-}
+const TimeInput = ({ value, handleInputChange}) => (
+  <div>
+    <label>Please select your required cleaning period:</label>
+    <input type="range" min="0.5" max="6" step="0.5"
+      defaultValue={ value }
+      onChange={ e => handleInputChange(e) }
+    />
+    <span>{ formatHours(value) }</span>
+  </div>
+)
 
 export default TimeInput
